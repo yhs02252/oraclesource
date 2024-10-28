76d24a57dd8280964a312d7dc3c1035e7d48b8d2
유창규 <yhs02252@gmail.com>
2024-10-28 18:02:36 +0900

    jpatable

---
+++ C##jpauser.sql
@@ -0,0 +1,12 @@
+-- memo 테이블 생성
+-- 메모번호(mno - pk), 메모내용(memo_text - not null)
+CREATE TABLE MEMO (
+	MNO NUMBER(20) PRIMARY KEY,
+	MEMO_TEXT VARCHAR(200) NOT NULL
+);
+
+-- 메모번호는 시퀀스(memo_seq) 입력
+CREATE SEQUENCE MEMO_SEQ;
+
+DROP TABLE MEMO;
+DROP SEQUENCE MEMO_SEQ;
